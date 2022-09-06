66473e3f108f33d031b2c1e72bc22a654d13a87a
alejandrodgz <5199@holbertonschool.com>
2022-09-06 16:49:13 -0500

    task 1 v3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,9 +9,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *ptr, *tmp, *tm2, *tm3, *tm4;
 	int value, count;
-
 	
-	if (*list == NULL || list == NULL)
+	if ((*list) == NULL || list == NULL)
 		return;
 	ptr = *list;
 	while (ptr->next != NULL)
