f0b0e49c21c003e099e033c68804ffafa9158fae
alejandrodgz <5199@holbertonschool.com>
2022-09-06 16:41:32 -0500

    task 1 v1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort double linked list
+ * @list: double linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *tmp, *tm2, *tm3, *tm4;
+	int value, count;
+
+	ptr = *list;
+	if (ptr == NULL)
+		return;
+	while (ptr->next != NULL)
+	{
+		count = 0;
+		tm2 = ptr;
+		tmp = tm2->next;
+		value = tmp->n;
+		while (tm2 != NULL && tm2->n > value)
+		{
+			count = 1;
+			tm4 = tmp->next;
+			tm3 = tm2;
+			tm2 = tm2->prev;
+			tm3->next = tmp->next;
+			tm3->prev = tmp;
+			tmp->next = tm3;
+			tmp->prev = tm2;
+			if (tm4 != NULL)
+				tm4->prev = tm3;
+			if (tm2 != NULL)
+				tm2->next = tmp;
+			if (tm2 == NULL)
+			{
+				*list = tmp;
+			}
+			value = tmp->n;
+			print_list(*list);
+		}
+		if (count == 0)
+			ptr = ptr->next;
+		else
+			ptr = ptr->prev;
+	}
+}
--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - Doubly linked list node
